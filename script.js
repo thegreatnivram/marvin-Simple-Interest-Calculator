5144ce75002319020a56d6aa3bd74902cf1e7498
thegreatnivram <marvinmejia@yahoo.com>
2022-09-03 21:48:04 -0600

    several changes
    several changes

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    const d = new Date();
+    let year = d.getFullYear();
+    rate = parseFloat(document.getElementById("tasa").value);
+    principal = parseFloat(document.getElementById("principal").value);
+    amountofyears = parseInt(document.getElementById("years").value);
+    targetyear = parseInt(year) + amountofyears; 
+    totalresult = Math.round(principal * ((rate / 100)* amountofyears));
+    document.getElementById("showprincipal").innerHTML = principal;
+    document.getElementById("showrate").innerHTML = rate;
+    document.getElementById("showyear").innerHTML = targetyear;
+    document.getElementById("showresult").innerHTML =  totalresult;
+    myFunction();
     
 }
+function show_value(x)
+{
+ document.getElementById("rate_value").innerHTML=x+"%";
+}           
+function myFunction() {
+    var x = document.getElementById("resulting");
+      x.style.display = "block";
+  }
         
